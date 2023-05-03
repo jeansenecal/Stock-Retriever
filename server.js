de56dc42dc7e79d61ad635bffb83699c7c883070
jeansenecal <jeansenecal02@gmail.com>
2023-05-02 21:29:07 -0400

    test2

--- server.js
+++ server.js
@@ -52,8 +52,8 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-//Scrape job that occurs everyday  0 0 11 ? * MON,TUE,WED,THU,FRI *
-const job = schedule.scheduleJob('0 26 21 * * 1,2,3,4,5', scrapeDayminer);
+//Scrape job that occurs everyday  0 26 21 * * 1,2,3,4,5
+const job = schedule.scheduleJob('0 26 21 * * *', scrapeDayminer);
 
 //Use flash messages for errors, info, ect...
 app.use(flash());
